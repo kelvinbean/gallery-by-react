b701d51120bf7944a394ca49702243251b3b844b
Bean <260254788@qq.com>
2016-11-22 10:18:12 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+/*
+ * Webpack development server configuration
+ *
+ * This file is set up for serving the webpack-dev-server, which will watch for changes and recompile as required if
+ * the subfolder /webpack-dev-server/ is visited. Visiting the root will not automatically reload.
+ */
+'use strict';
+var webpack = require('webpack');
+
+module.exports = {
+
+  output: {
+    filename: 'main.js',
+    publicPath: '/assets/'
+  },
+
+  cache: true,
+  debug: true,
+  devtool: 'sourcemap',
+  entry: [
+      'webpack/hot/only-dev-server',//这是个处理热更新的文件。需要gruntfile那边的hot属性设置为true
+      './src/components/GalleryByReactApp.js'
+  ],
+
+  stats: {
+    colors: true,
+    reasons: true
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      'styles': __dirname + '/src/styles',
+      'mixins': __dirname + '/src/mixins',
+      'components': __dirname + '/src/components/'
+    }
+  },
+  module: {
+    preLoaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader'
+    }],
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel-loader'
+    }, {
+      test: /\.scss/,
+      loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded'
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader!postcss-loader'
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }, {
+      test: /\.(png|jpg|woff|woff2)$/,
+      loader: 'url-loader?limit=8192'
+    }]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+
+};
---
+++ webpack.dist.config.js
@@ -0,0 +1,69 @@
+/*
+ * Webpack distribution configuration
+ *
+ * This file is set up for serving the distribution version. It will be compiled to dist/ by default
+ */
+
+'use strict';
+
+var webpack = require('webpack');
+
+module.exports = {
+
+  output: {
+    publicPath: '/assets/',//部署到服务器的路径
+    path: 'dist/assets/',//输出到项目路径
+    filename: 'main.js'//生成该文件名
+  },
+
+  debug: false,
+  devtool: false,
+  entry: './src/components/GalleryByReactApp.js',
+
+  stats: {
+    colors: true,
+    reasons: false
+  },
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      'styles': __dirname + '/src/styles',
+      'mixins': __dirname + '/src/mixins',
+      'components': __dirname + '/src/components/'
+    }
+  },
+
+  module: {
+    preLoaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader'
+    }],
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader!postcss-loader'
+    }, {
+      test: /\.scss/,
+      loader: 'style-loader!css-loader!postcss-loader!sass-loader?outputStyle=expanded'
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }, {
+      test: /\.(png|jpg|woff|woff2)$/,
+      loader: 'url-loader?limit=8192'
+    }]
+  }
+};
